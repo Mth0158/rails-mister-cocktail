3f48180efc8639fb84b3f362580f0a5b10e025a1
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-03-05 12:15:18 +0100

    change config asset compile to true

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: %i[new create index show] do
+    resources :doses, only: %i[new create]
+  end
+
+  resources :doses, only: %i[destroy]
+
+  # get '/cocktails/:id/edit', to: 'cocktails#edit', as: :edit_cocktail
+  # patch '/cocktails/:id', to: 'cocktails#update'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredients = JSON.parse(ingredient_serialized)
+
+puts 'Cleaning the previous DB'
+Ingredient.destroy_all
+
+puts "Seeding the DB with #{ingredients['drinks'].count} ingredients"
+ingredients['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+puts "Created #{Ingredient.all.count} ingredients!"
